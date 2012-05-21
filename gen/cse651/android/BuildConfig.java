a728b3f763b2a1d665b32ed1320eae51a6e5fad2
Me <moxx@BEAST.(none)>
2012-05-20 20:47:50 -0400

    Initial commit.

---
+++ gen/cse651/android/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package cse651.android;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
---
+++ src/cse651/android/Cse651Activity.java
@@ -0,0 +1,13 @@
+package cse651.android;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Cse651Activity extends Activity {
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
